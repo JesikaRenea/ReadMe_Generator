dfe6224728c387c736f7b9a5ba7ff546c9ba67e2
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-02-28 22:13:36 -0500

    Generated Question Prompts

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# ReadMe_Generator
-Good README Generator
+
+# Your Project Title
+
+
+## Description 
+t
+
+##User Story
+
+As a <tt> I can <t>, so that I can<t>
+
+
+## Table of Contents
+
+* [Installation](#t)
+
+* [Usage](#t)
+
+* [Credits](#t)
+
+* [License](#t)
+
+
+---
+
+## Badges
+
+undefined
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## Contributing
+undefined
+
+
+## Tests
+undefined
+
+
+---
--- index.js
+++ index.js
@@ -1,12 +1,147 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
-}
+// const questions = [
 
-function init() {
+// ];
 
-}
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Enter Project Title:"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Enter Project Description:"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Enter Installation Requirements:"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Enter Project Usage:"
+      },      {
+        type: "input",
+        name: "credits",
+        message: "Credits:"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "License:"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "GitHub Username:"
+      },
+      {
+        type: "input",
+        name: "github-picture",
+        message: "GitHub Profile Picture URL:"
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "User Story: Enter Role of User"
+      },
+      {
+        type: "input",
+        name: "capabilities",
+        message: "User Story: Enter Project Capabilties"
+      },
+      {
+        type: "input",
+        name: "benefits",
+        message: "User Story: Enter Project Benefits"
+      },
+    ])
+    // .then(answers => {
+    //     console.log(answers);
+    //   });
+  };
 
-init();
+
+  function generateReadMe(answers) {
+    return `
+# Your Project Title
+${answers.title}
+
+## Description 
+${answers.description}
+
+##User Story
+
+As a <${answers.role}> I can <${answers.capabilities}>, so that I can<${answers.benefits}>
+
+
+## Table of Contents
+
+* [Installation](#${answers.installation})
+
+* [Usage](#${answers.usage})
+
+* [Credits](#${answers.credits})
+
+* [License](#${answers.license})
+
+
+---
+
+## Badges
+
+${answers.badges}
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+
+## Contributing
+${answers.contributing}
+
+
+## Tests
+${answers.tests}
+
+
+---
+`
+  };
+
+  
+promptUser()
+.then(function(answers) {
+  const readMe = generateReadMe(answers);
+
+  return writeFileAsync("README.md", readMe);
+})
+.then(function() {
+  console.log("Successfully wrote to README.md");
+})
+.catch(function(err) {
+  console.log(err);
+});
+
+
+
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
+
+
